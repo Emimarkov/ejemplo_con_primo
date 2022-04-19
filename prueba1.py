4b7556bf805532514478fee331208fa19297f162
Pablo Bernasconi <pablonicolas17@gmail.com>
2022-04-18 22:16:00 -0300

    Agrego 2 lineas

--- prueba1.py
+++ prueba1.py
@@ -4,3 +4,6 @@ Esta una prueba para ver si funciona trabajar en conjunto con otra persona.
 
 print("Mi Primo es crack")
 print("porque el me ayuda a que aprenda a programar")
+
+print("Te voy a sacar bueno jajaja")
+print("Y vamos a programar una app millonaria")
